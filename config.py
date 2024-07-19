dcc525ab9f4f7df31479adbad419cd0e0c18521c
khufiyajasuse <140427665+khufiyajasuse@users.noreply.github.com>
2024-07-19 19:46:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -29,7 +29,7 @@ PORT = os.environ.get("PORT", "8080")
 
 #Database 
 DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://ashwinimalaysian:5gRvQgPW4DRhlEpE@pehla.uuzwevb.mongodb.net/?retryWrites=true&w=majority&appName=pehla")
-DB_NAME = os.environ.get("DATABASE_NAME", "ashwinimalaysian")
+DB_NAME = os.environ.get("DATABASE_NAME", "pehla")
 
 #force sub channel id, if you want enable force sub
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
